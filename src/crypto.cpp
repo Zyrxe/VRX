bab6c9586458915caad17fed9f52929026f01128
Sel <sofia.sry7@gmail.com>
2025-09-24 13:37:17 +0700

    Create crypto.cpp

---
+++ src/crypto.cpp
@@ -0,0 +1,13 @@
+#include "crypto.h"
+#include <openssl/sha.h>
+#include <iomanip>
+#include <sstream>
+
+std::string sha256(const std::string str) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256((unsigned char*)str.c_str(), str.size(), hash);
+    std::stringstream ss;
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+    return ss.str();
+}
